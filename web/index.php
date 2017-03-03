418aeb6946625bb16e3b8dceb145b2aeb2bc427c
Akbar Hirani <akbar.hirani123@gmail.com>
2017-03-03 16:46:36 -0500

    Angular JS ng-view for stateProvider

--- web/index.php
+++ web/index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE>
-<html ng-app>
+<html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>AngularJS App 1 | By Akbar Hirani</title>
   <link rel="stylesheet" href="/resources/css/style.css">
 </head>
-<body style="min-height: 2000px;">
+<body style="min-height: 2000px;" ng-app="myApp">
 	<header>
 		<div class="navbar">
 			<div class="navbar-brand">
-				<li><a href="/">Akbar Hirani</a></li>
+				<li><a ui-sref="#">Akbar Hirani</a></li>
 			</div>
 			<ul class="navbar" id="mynavbar">
-			  <li><a class="active" href="#home">Home</a></li>
+			  <li><a class="active" ui-sref="home">Home</a></li>
 			  <li><a href="#news">News</a></li>
 			  <li><a href="#contact">Contact</a></li>
-			  <li><a href="#about">About</a></li>
+			  <li><a ui-sref="about">About</a></li>
 			  <li class="icon">
 			    <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">☰</a>
 			  </li>
