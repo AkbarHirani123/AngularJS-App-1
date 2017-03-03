e57365481a2d2ef7de8ee42a2d6abeffb984e57e
Akbar Hirani <akbar.hirani123@gmail.com>
2017-03-02 23:44:10 -0500

    Created a basic responsive navbar from scratch.

--- web/index.php
+++ web/index.php
@@ -1,7 +1,27 @@
 <!DOCTYPE>
 <html>
 <head>
-<title>Hello World!</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AngularJS App 1 | By Akbar Hirani</title>
+<link rel="stylesheet" href="css/style.css">
+<script src="js/script.js"></script>
 </head>
-<body>Hello World!</body>
+<body style="min-height: 2000px;">
+	<header class="navbar">
+		<div class="navbar">
+			<div class="navbar-header">
+				<a class="navbar-brand">Akbar Hirani</a>
+			</div>
+			<ul class="navbar" id="myTopnav">
+				<li><a href="/web">Home</a></li>
+				<li><a href="/">Back To Akbar Hirani</a></li>
+				<li><a href="/">About</a></li>
+				<li class="icon">
+				  <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
+				</li>
+			</ul>
+		</div>
+	</header>
+	<div class="content">HELLO WORLD!</div>
+</body>
 </html>
